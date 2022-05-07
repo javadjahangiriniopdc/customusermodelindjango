55c14a295b3457067b8b91012256d881207171d6
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-05-07 00:39:24 -0700

    add user account and user account manager

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('user must have an email')
+        else:
+            email = self.normalize_email(email)
+            user = self.model(email=email, name=name)
+            user.set_password(password)
+            user.save()
+            return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email=email, name=name, password=password)
+        user.is_admin = True
+        user.is_active = True
+        user.save()
+        return user
+
+
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
